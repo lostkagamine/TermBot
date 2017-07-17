e2ccf6ef3a5593f45abd9e7b666a10d96f2a49ca
martmists <mail@martmists.com>
2017-07-17 14:58:58 +0000

    Update test.py

--- extensions/test.py
+++ extensions/test.py
@@ -1,15 +1,13 @@
 import discord
 from discord.ext import commands
 
+
 class TestExtension():
     def __init__(self, bot):
         self.bot = bot
-    
-    def setup(bot):
-        bot.add_cog(TestExtension(bot))
 
-    @self.bot.command()
-    async def test(ctx):
+    @commands.command()
+    async def test(self, ctx):
         await ctx.send("Hi from a cog.")
 
 
