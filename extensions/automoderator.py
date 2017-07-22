4495cdc96222439938cba71930fd868344de758e
ry00001 <dar.gargano@gmail.com>
2017-07-22 15:48:44 +0100

    aaaaaaAAA

--- extensions/automoderator.py
+++ extensions/automoderator.py
@@ -8,12 +8,13 @@ regex = "(?:discord(?:(?:\.|.?dot.?)gg|app(?:\.|.?dot.?)com\/invite)\/(([\w]{1,}
 class AntiAdvertising():
             
     @commands.command()
-    async def automod_setup(ctx, *args):
-        if args[0] == None:
+    async def automodsetup(ctx, *args):
+        print("ran")
+        if args == None:
             return
         if args[0] == "invite":
             if args[1] == None:
-                await ctx.send("Syntax: `automod_setup invite <ban/kick/delete>`")
+                await ctx.send("Syntax: `automodsetup invite <ban/kick/delete>`")
                 try:
                     t = r.db("termbot").table("automoderator_invites")
                 except rethinkdb.errors.ReqlOpFailedError:
